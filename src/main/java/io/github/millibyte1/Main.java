b09608669aa0daa6d06546770d0eba6e9641fc4e
Evan Hall <ebhall99@gmail.com>
2022-12-09 15:45:10 -0500

    first commit

---
+++ src/main/java/io/github/millibyte1/Main.java
@@ -0,0 +1,12 @@
+package io.github.millibyte1;
+
+public class Main {
+    public static void main(String[] args) {
+        foo();
+    }
+
+    /** hello world */
+    public static void foo() {
+        System.out.println("Hello world!");
+    }
+}
